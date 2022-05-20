a39140f2cc222468b1241272afb0d8970c4c466f
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-05-20 20:11:30 +0530

    update

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url= 'https://api.npoint.io/93244e43ca50c8fbdf2e';
+const url= 'https://aryamitra06.github.io/projectsjson/projects.json';
 
 export const getAllProjects = async () => {
     const res = await axios.get(`${url}`);
